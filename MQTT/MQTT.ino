62e8e086c485b39a302110c0a044e381667c289e
Michael Harditya <michael.harditya-intern@prasimax.com>
2022-11-24 19:49:25 +0700

    Verified MQTT Module

--- MQTT/MQTT.ino
+++ MQTT/MQTT.ino
@@ -1,17 +1,20 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 //WiFi Credential, adjust
-const char *SSID = "wifi_ssid"
-const char *PWD = "wifi_pass"
+const char *SSID = "yak2";
+const char *PWD = "4R67$8r2";
 //MQTT Credential
 WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-char *mqttServer = "broker.hivemq.com";
+PubSubClient client(wifiClient);
+char *mqttServer = "c0c46a42f7444eb19e7fc7bcda2cd313.s2.eu.hivemq.cloud";
 int mqttPort = 1883;
 //device Credential, adjust
-const char *deviceId = "ESP32-testing";
+const char* deviceId = "ESP32-testing";
+//message buffer
+#define MSG_BUFFER_SIZE  50
+char datamsg[MSG_BUFFER_SIZE];
 void setupMQTT() {
-  mqttClient.setServer(mqttServer,mqttPort);
+  client.setServer(mqttServer,mqttPort);
 }
 void connectWiFi() {
   Serial.print("Connecting to ");
@@ -21,18 +24,19 @@ void connectWiFi() {
     Serial.print(".");
     delay(500);
   }
-  Serial.print("Connected!");
+  Serial.println("Connected!");
+  Serial.println(WiFi.localIP());
 }
 void reconnect(){
   Serial.println("Connecting to MQTT Broker");
   String clientId = "client-id"; //adjust
   clientId += String(random(0xffff),HEX);
-  if (mqttClient.connect(clientId.c_str())){
+  if (client.connect(clientId.c_str())){
     Serial.println("Connected");
     //subscribe the topic
-    String topic = "/data/";
-    topic += deviceId;
-    mqttClient.subscribe(topic);
+    char* topic = "/data/";
+    topic = strcat(topic,deviceId);
+    client.subscribe(topic);
   }
   
 }
@@ -46,25 +50,26 @@ void loop() {
   // set MQTT server
   setupMQTT();
   // check conectivity, if it is not connected, then it will try to reconnect
-  if(!mqttClient.conected()) reconnect();
-  mqttClient.loop();
+  if(!client.connected()) reconnect();
+  client.loop();
   // send data to broker (publish)
-  String topic = "/data/" + deviceId;
+  char* topic = strcat("/data/",deviceId);
   long lastTime = 0;
   long timeNow = millis();
   if(timeNow - lastTime > 60000){
     int tds = 1000;
     int waterLevel = 20;
     //send tds
-    topic += "/tds";
-    sprintf(data,"%d",tds);
-    Serial.println(data);
-    mqttClient.publish(topic,data);
+    topic = strcat(topic,"/tds");
+    sprintf(datamsg,"%d",tds);
+    Serial.println(datamsg);
+    client.publish(topic,datamsg);
     //send waterLevel
-    topic = "/data/" + deviceId + "/waterlvl";
-    sprintf(data,"%d",waterLevel);
-    Serial.println(data);
-    mqttClient.publish(topic,data);
+    topic = strcat("/data/",deviceId);
+    topic = strcat(topic,"/waterlvl");
+    sprintf(datamsg,"%d",waterLevel);
+    Serial.println(datamsg);
+    client.publish(topic,datamsg);
     lastTime = timeNow;
   }
 }
