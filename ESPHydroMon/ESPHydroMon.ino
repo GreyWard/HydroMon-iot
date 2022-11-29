935d8093a896b653a55255936d4ff8a034e313bc
Michael Harditya <michael.harditya-intern@prasimax.com>
2022-11-29 18:32:52 +0700

    Added Deep Sleep feature and updated README

--- ESPHydroMon/ESPHydroMon.ino
+++ ESPHydroMon/ESPHydroMon.ino
@@ -16,10 +16,11 @@
 //Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
+/* Device Credentials */
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyBpbe1_pyHkzX388NJrgHt9bxuNMCtUct8"
 
-// Insert RTDB URLefine the RTDB URL */
+// Insert RTDB URL
 #define DATABASE_URL "https://hydromon-f29cc-default-rtdb.asia-southeast1.firebasedatabase.app/" 
 
 // Insert your network credentials
@@ -27,9 +28,9 @@
 #define WIFI_PASSWORD "Blue@231"
 
 // Defining Pins
-#define echoPin 2
-#define trigPin 0
-#define resetPin 15
+#define echoPin 34
+#define trigPin 35
+#define resetPin 14
 #define TdsSensorPin 27
 #define probePin 29 
 
@@ -37,8 +38,8 @@
 #define EEPROM_SIZE 1
 #define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP  360        /* Time ESP32 will go to sleep (in seconds) */
-#define VREF 3.3              // analog reference voltage(Volt) of the ADC
-#define SCOUNT  30            // sum of sample point
+#define VREF 3.3              /* analog reference voltage(Volt) of the ADC */
+#define SCOUNT  30            /* sum of sample point */
 //RTC_DATA_ATTR int bootCount = 0;
 
 //Global Variables
@@ -66,7 +67,124 @@ unsigned long sendDataPrevMillis = 0;
 int count = 0;
 bool signupOK = false;
 
-// median filtering algorithm
+/* sleep function for ESP32
+ * can be woke up by interrupt pin (14) or time until 1 hour
+ * param none
+ * return none
+ */
+void goingToSleep(){
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_14,1); //1 = High, 0 = Low
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Going to Sleep");
+  Serial.flush();
+  esp_deep_sleep_start(); //code starting from here wont be run
+}
+/* Wakeup function for ESP32
+ * for debugging purposes, shows what woke the ESP32
+ */
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    default : Serial.printf("Wakeup was caused by other: %d\n",wakeup_reason); break;
+  }
+}
+/* Firebase initialization
+ * open connection to Firebase database
+ */
+void firebase_init(){
+  // Connect to WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "f617fb2ded7eb9a85da9e53ad1a8224d2acff01196b9e6075931d1e8099164a5anon.com", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+/* Send data to Firebase
+ * send two data, tds and water level to Firebase
+ * @param tds(int)
+ * @param level(float)
+ */
+void send_firebase(int tds, float level){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "data/tds", tds)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    count++;
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, "data/level", 0.01 + level)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
+/* EEPROM initialization
+ * saves distance between sensor to the bottom of the tank
+ */
+ void eeprom_init(){
+  //Starting EEPROM
+  if(!EEPROM.begin(EEPROM_SIZE)){
+    Serial.println("Failed to initialize BOTTOMDIST");
+    Serial.println("Restarting");
+    delay(1000);
+    ESP.restart();
+  }
+  //Check the last measurement of bottom tank distance, if no data then save new one
+  bottomDistance = EEPROM.read(0);
+  if(bottomDistance == -1){
+    bottomDistance = takeDistance();
+    EEPROM.write(0,bottomDistance);
+    EEPROM.commit();
+    Serial.print("Put new Bottom Distance: ");
+    Serial.println(bottomDistance);
+  } else {
+    Serial.print("Taken Bottom Distance: ");
+    Serial.println(bottomDistance);
+  }
+ }
+/* median filtering algorithm
+ * to get representative data from 30 measurements
+ * @param bArray[](int)
+ * @param iFilterLen(int)
+ */
 int getMedianNum(int bArray[], int iFilterLen){
   int bTab[iFilterLen];
   for (byte i = 0; i<iFilterLen; i++)
@@ -90,7 +208,9 @@ int getMedianNum(int bArray[], int iFilterLen){
   return bTemp;
 }
 
-// read data from ultrasonic sensor, in centimeter
+/* read data from ultrasonic sensor, in centimeter
+ * @return distance(int)
+ */
 int takeDistance(){
   //clear trigPin
   digitalWrite(trigPin,LOW);
@@ -108,56 +228,7 @@ int takeDistance(){
   distance = duration * 0.034 / 2;
   return distance;
 }
-//sleep function for ESP32
-void goingToSleep(){
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  Serial.println("Going to Sleep");
-  Serial.flush();
-  esp_deep_sleep_start(); //code starting from here wont be run
-}
-//wakeup func
-void print_wakeup_reason(){
-  esp_sleep_wakeup_cause_t wakeup_reason;
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-  switch(wakeup_reason)
-  {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
-  }
-}
 
-// Send data to Firebase
-void send_firebase(int tds, float level){
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
-    sendDataPrevMillis = millis();
-    // Write an Int number on the database path test/int
-    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-    count++;
-    
-    // Write an Float number on the database path test/float
-    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
-      Serial.println("PASSED");
-      Serial.println("PATH: " + fbdo.dataPath());
-      Serial.println("TYPE: " + fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.println("REASON: " + fbdo.errorReason());
-    }
-  }
-}
 void setup(){
   Serial.begin(115200);
   print_wakeup_reason(); //for debugging sleep function
@@ -169,60 +240,21 @@ void setup(){
   Serial.begin(9600);
   Serial.println("Ultrasonic Sensor Test");
   //Starting EEPROM
-  if(!EEPROM.begin(EEPROM_SIZE)){
-    Serial.println("Failed to initialize BOTTOMDIST");
-    Serial.println("Restarting");
-    delay(1000);
-    ESP.restart();
-  }
-  //Check the last measurement of bottom tank distance, if no data then save new one
-  bottomDistance = EEPROM.read(0);
-  if(bottomDistance == -1){
+  eeprom_init();
+  //Starting Firebase connection
+  firebase_init();
+}
+
+void loop(){
+  static unsigned long analogSampleTimepoint = millis();
+  // when the reset button pressed, take new bottom distance
+  if (!digitalRead(resetPin)){
     bottomDistance = takeDistance();
     EEPROM.write(0,bottomDistance);
     EEPROM.commit();
     Serial.print("Put new Bottom Distance: ");
     Serial.println(bottomDistance);
-  } else {
-    Serial.print("Taken Bottom Distance: ");
-    Serial.println(bottomDistance);
   }
-  // Connect to WiFi
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
-  /* Assign the api key (required) */
-  config.api_key = API_KEY;
-
-  /* Assign the RTDB URL (required) */
-  config.database_url = DATABASE_URL;
-
-  /* Sign up */
-  if (Firebase.signUp(&config, &auth, "f617fb2ded7eb9a85da9e53ad1a8224d2acff01196b9e6075931d1e8099164a5anon.com", "")){
-    Serial.println("ok");
-    signupOK = true;
-  }
-  else{
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
-
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
-  
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
-}
-
-void loop(){
-  static unsigned long analogSampleTimepoint = millis();
   // measure ppm
   if(millis()-analogSampleTimepoint > 40U){     //every 40 milliseconds,read the analog value from the ADC
     analogSampleTimepoint = millis();
@@ -231,8 +263,8 @@ void loop(){
     if(analogBufferIndex == SCOUNT){ 
       analogBufferIndex = 0;
     }
-  }   
-  
+  }
+  // after 30 tds value measurements
   static unsigned long printTimepoint = millis();
   if(millis()-printTimepoint > 800U){
     printTimepoint = millis();
@@ -255,30 +287,18 @@ void loop(){
       Serial.print(tdsValue,0);
       Serial.println("ppm");
     }
-  }
 
   //measure water level
-  static unsigned long distanceSampleTimepoint = millis();
-  if (millis() - distanceSampleTimepoint>4000U){
-    distanceSampleTimepoint = millis();
-    waterLevel = bottomDistance - takeDistance();
-    //the result
-    Serial.print("Water Level: ");
-    Serial.println(waterLevel);
-    Serial.print("Taken Bottom Distance: ");
-    Serial.println(bottomDistance);
-  }
-  // when the reset button pressed, take new bottom distance
-  if (!digitalRead(resetPin)){
-    bottomDistance = takeDistance();
-    EEPROM.write(0,bottomDistance);
-    EEPROM.commit();
-    Serial.print("Put new Bottom Distance: ");
-    Serial.println(bottomDistance);
-  }
+  waterLevel = bottomDistance - takeDistance();
+  //the result
+  Serial.print("Water Level: ");
+  Serial.println(waterLevel);
+  Serial.print("Taken Bottom Distance: ");
+  Serial.println(bottomDistance);
+  
   // send data to firebase
   send_firebase(tdsValue,waterLevel);
   // going to sleep
-  //goingToSleep();
-  
+  goingToSleep();
+  }
 }
