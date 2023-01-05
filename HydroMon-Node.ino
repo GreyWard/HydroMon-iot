5586acaed3228dfff6dd299a7779445490b723d7
GreyWard <michael.harditya@ui.ac.id>
2023-01-05 16:19:59 +0700

    Replaced connectivity with HTTP

--- HydroMon-Node.ino
+++ HydroMon-Node.ino
@@ -11,13 +11,17 @@
 #define BLYNK_AUTH_TOKEN "MAIgQDV71UPoVE9L8KjSrOiDPTCcZOQK"
 
 // Libraries
-#include <Arduino.h>
-#include "Adafruit_ADS1X15.h"
-#include "EEPROM.h"  
-#include "DFRobot_ESP_EC.h"
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <esp_now.h>
+#include <HTTPClient.h>
+#include "Adafruit_ADS1X15.h"
+#include <Arduino.h>
+#include "DFRobot_ESP_EC.h"
+
+// Insert your network credentials
+#define AP_SSID "BaS3Station"
+#define AP_PASS "Bas3b@ll"
+
+const char* serverName = "http://192.168.4.1/post";
 
 //TDS Initiation
 DFRobot_ESP_EC ec;
@@ -27,65 +31,18 @@ float voltage, ecValue, temperature = 25;
 //Sensor Pins
 #define echoPin 12
 #define trigPin 13
-#define TdsSensorPin 27
-#define probePin 29           // to release voltage to the water level sensor
-
-//Constants
-#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define VREF 3.3              // analog reference voltage(Volt) of the ADC
-#define SCOUNT  30            // sum of sample point
+#define TdsSensorPin A0
 
 //Sensor Values
 long duration;
 int distance;
 float averageVoltage = 0;
-float tdsValue = 0;
+int tdsValue = 0;
 
 // Water Level variables, store the distance value in the array
-int bottomDistance = 0;
+int bottomDistance = 200;
 int waterLevel = 0;
-//controller variables
-bool remeasuring = false;
 
-//message structure
-typedef struct struct_message {
-    int ID;
-    float tds;
-    int water;
-} struct_message;
-//struct to hold control data
-typedef struct struct_control {
-  int ID;
-  bool remeasure;
-}crtl;
-crtl controlMessage;
-struct_message message;
-
-uint8_t broadcastAddress[] = {0x7C, 0x9E, 0xBD, 0x37, 0xCA, 0x84};
-
-//Task initialization
-TaskHandle_t Task1;
-TaskHandle_t Task2;
-
-void data_sent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\n Last Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
-
-//Callback for received data from nodes with ESP NOW
-void data_receive(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
-  char macStr[18];
-  Serial.print("Received from: ");
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.println(macStr);
-  memcpy(&controlMessage, incomingData, sizeof(controlMessage));
-  remeasuring = controlMessage.remeasure;
-}
-
-/* read data from ultrasonic sensor, in centimeter
- * @return distance(int)
- */
 int takeDistance(){
   //clear trigPin
   digitalWrite(trigPin,LOW);
@@ -104,99 +61,62 @@ int takeDistance(){
   return distance;
 }
 
-void sensorTask(void *pvParameter) {
-  for(;;){
-    //read tds value
-    voltage = analogRead(A0);
-      //Serial.print("voltage:");
-      //Serial.println(voltage, 4);
+void postData() {
+  HTTPClient http;
 
-      ecValue = ec.readEC(voltage, temperature); // convert voltage to EC with temperature compensation
-      tdsValue = ecValue *900; //approximately 1 ec = 900ppm
-      Serial.print("EC:");
-      //Serial.print(ecValue, 4);
-      //Serial.println("ms/cm");
-      Serial.println(ecValue); 
-      Serial.println(tdsValue); 
-    //read water level
-    if (remeasuring == true){
-      bottomDistance = takeDistance();
-      Serial.print("Put new Bottom Distance (Reset Pressed): ");
-      Serial.println(bottomDistance);
-      delay(1000);
-    }else{
-      waterLevel = bottomDistance - takeDistance();
-      Serial.print("Water Level: ");
-      Serial.println(waterLevel);
-    }
-    ec.calibration(voltage,temperature);
-    delay(5000);
+  http.begin(serverName); // specify the POST endpoint
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded"); // set the content-type header
+
+  // create a POST request payload
+  String payload = "tds="+String(tdsValue)+"&water="+String(waterLevel);
+  int httpCode = http.POST(payload); // send the POST request
+
+  if (httpCode > 0) {
+    // check the status code
+    String response = http.getString(); // get the response payload
+    Serial.println(httpCode); // print the status code
+    Serial.println(response); // print the response payload
+  } else {
+    Serial.println("Error: " + http.errorToString(httpCode));
   }
+
+  http.end(); // close the connection
 }
 
-void sendTask(void *pvParameter){
-  for (;;){
-    message.ID = 1;
-    message.tds = tdsValue;
-    message.water = waterLevel;
-
-    esp_err_t outcome = esp_now_send(broadcastAddress, (uint8_t *) &message, sizeof(message));
-    
-    if (outcome == ESP_OK) {
-      Serial.println("Sent with success");
-    }
-    else {
-      Serial.println("Error sending the data");
-    }
-    delay(10000);
-  }
+void dataProcess() {
+  //read tds value
+  voltage = analogRead(TdsSensorPin);
+  ecValue = ec.readEC(voltage, temperature); // convert voltage to EC with temperature compensation
+  tdsValue = ecValue *900; //approximately 1 ec = 900ppm
+  Serial.print("TDS:");
+  Serial.println(tdsValue); 
+  //read water level
+  waterLevel = bottomDistance - takeDistance();
+  Serial.print("Water Level: ");
+  Serial.println(waterLevel);
+  postData();
+  ec.calibration(voltage,temperature);
 }
 
 void setup(){
   Serial.begin(115200);
-  // connect ESP NOW
-  WiFi.mode(WIFI_STA);
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  esp_now_register_send_cb(data_sent);
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
-  // Register for a callback function that will be called when data is received
-  esp_now_register_recv_cb(data_receive);
   pinMode(TdsSensorPin,INPUT);
-  pinMode(probePin,OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.println("Connecting");
-  EEPROM.begin(32);
+  WiFi.begin(AP_SSID,AP_PASS);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
   ec.begin();
-  xTaskCreatePinnedToCore(
-  sendTask,
-  "Data Send Task",
-  10000,
-  NULL,
-  1,
-  &Task1,
-  0);
-  delay(500);
-xTaskCreatePinnedToCore(
-  sensorTask,
-  "Sensor Task",
-  10000,
-  NULL,
-  1,
-  &Task2,
-  1);
-  delay(500);
 }
 
-void loop(){ //we already divided the tasks above
+void loop(){
+  dataProcess();
+  delay(5000);
 }
